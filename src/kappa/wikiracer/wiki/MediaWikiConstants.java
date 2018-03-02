fa352848fa5e99bcb755a06a13c5468a2e0a99e4
UTSCMatt <matthewyathin.lau@mail.utoronto.ca>
2018-03-02 18:36:04 -0500

    Fixed constants not being final

--- src/kappa/wikiracer/wiki/MediaWikiConstants.java
+++ src/kappa/wikiracer/wiki/MediaWikiConstants.java
@@ -1,10 +1,10 @@
 package kappa.wikiracer.wiki;
 
 public class MediaWikiConstants {
-  public static String QUERY = "query";
-  public static String PAGES = "pages";
-  public static String LINKS = "links";
-  public static String TITLE = "title";
-  public static String CONTINUE = "continue";
-  public static String ACCONTINUE = "accontinue";
+  public static final String QUERY = "query";
+  public static final String PAGES = "pages";
+  public static final String LINKS = "links";
+  public static final String TITLE = "title";
+  public static final String CONTINUE = "continue";
+  public static final String ACCONTINUE = "accontinue";
 }
