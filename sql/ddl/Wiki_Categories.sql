3a04bed1e385da7d13bd74456bf58577a93c9d5d
Matthew Lau <matthewyathin.lau@mail.utoronto.ca>
2018-03-17 18:00:07 -0400

    Added sql for category rules

---
+++ sql/ddl/Wiki_Categories.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE Wiki_Categories (
+	Id INT NOT NULL AUTO_INCREMENT,
+	Category VARCHAR(255) NOT NULL,
+	PRIMARY KEY (Id, Category)
+);
---
+++ sql/dml/ban_category.sql
@@ -0,0 +1,12 @@
+﻿DELIMITER //
+
+CREATE PROCEDURE Ban_Category (newGameId VARCHAR(255), newCategory VARCHAR(255))
+BEGIN
+	DECLARE realGameId, realCategoryId INT;
+	IF (SELECT COUNT(Id) FROM Wiki_Categories WHERE Category=newCategory) < 1 THEN
+		INSERT INTO Wiki_Categories (Category) Values (newCategory);
+	END IF;
+	SET realGameId = (SELECT Id FROM Games WHERE GameId=newGameId);
+	SET realCategoryId = (SELECT Id FROM Wiki_Categories WHERE Category=newCategory);
+	INSERT INTO banned_categories (GameId, CategoryId) Values (newGameId, realCategoryId);
+END //
