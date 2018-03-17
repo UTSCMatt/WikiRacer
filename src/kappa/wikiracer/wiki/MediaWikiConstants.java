d567b0dd0a2deca83d43625482ef6e2ab0996666
Matthew Lau <matthewyathin.lau@mail.utoronto.ca>
2018-03-17 16:51:30 -0400

    Added method to check if page belongs to category

--- src/kappa/wikiracer/wiki/MediaWikiConstants.java
+++ src/kappa/wikiracer/wiki/MediaWikiConstants.java
@@ -9,4 +9,5 @@ public class MediaWikiConstants {
   public static final String ACCONTINUE = "accontinue";
   public static final String MISSING_ID = "-1";
   public static final String RANDOM = "random";
+  public static final String CATEGORIES = "categories";
 }
